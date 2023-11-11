57a10004948feddd21e712a565ad5934878e468e
alejopua <alejopua@gmail.com>
2023-11-11 15:13:42 -0500

    [feat]: Create async functions in the users.store.js

---
+++ src/users/store/users.store.js
@@ -0,0 +1,29 @@
+const state = {
+    CurrentPage: 0,
+    Users: [],
+}
+
+const loadNextPage = async() => {
+
+}
+
+const loadPreviousPage = async() => {
+
+}
+
+const onUserChange = async() => {
+
+}
+
+const reloadPage = async() => {
+
+}
+
+export default {
+    loadNextPage,
+    loadPreviousPage,
+    onUserChange,
+    reloadPage,
+}
+
+    
