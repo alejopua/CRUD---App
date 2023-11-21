5dbdd4b39cf998bf9057e5f8a82e20cb8cdccb78
alejopua <alejopua@gmail.com>
2023-11-21 10:38:55 -0500

    [feature](app): add renders

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import { RenderAddButton } from "./presentation/render-add-button/render-add-button";
+import { RenderButton } from "./presentation/render-button/render.button";
+import { RenderTable } from "./presentation/render-table/render.table";
 import usersStore from "./store/users.store";
 
 /**
@@ -7,5 +10,11 @@ import usersStore from "./store/users.store";
 export const app = async( element ) => {
     element.innerHTML = 'loading...';
     await usersStore.loadNextPage();
+    // console.log(usersStore.getUsers())
+    element.innerHTML = '';
+    console.log(usersStore.getUsers())
 
+    RenderTable(element);
+    RenderButton(element);
+    RenderAddButton(element);
 }
