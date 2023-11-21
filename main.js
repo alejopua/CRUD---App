92e2fb23914b85e12e9f718f5a9661e7d779fb66
alejopua <alejopua@gmail.com>
2023-11-21 10:39:42 -0500

    [feature](main): modify

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+import { app } from './src/app'
 import './style.css'
 document.querySelector('#app').innerHTML = `
-  <div>
+  <div id="renderApp">
 
   </div>
 `
+const element = document.querySelector('#renderApp');
+app(element);
