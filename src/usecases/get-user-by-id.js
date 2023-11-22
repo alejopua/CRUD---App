348a4746e2b2b34f67fbb95756395887c512e3fd
alejopua <alejopua@gmail.com>
2023-11-22 14:17:19 -0500

    [feature](create): use cases method http get

---
+++ src/usecases/get-user-by-id.js
@@ -0,0 +1,14 @@
+import { lHostUserMapper } from "../mappers/localhost-user-mapper";
+
+/**
+ * 
+ * @param {String|Number} id
+ * @returns {Promise<User>}
+ */
+export const getUserById = async( id ) => {
+    const url = `${ import.meta.env.VITE_BASE_URL }/users/${ id }`;
+    const res = await fetch( url );
+    const data = await res.json();
+    const user = lHostUserMapper(data);
+    return user;
+}
