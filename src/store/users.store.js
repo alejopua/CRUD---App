8a199621245e5168c09322c50e522d6848ae452b
alejopua <alejopua@gmail.com>
2023-11-22 14:15:41 -0500

    [feature](implement): ad logic structure at onUserChange and reloadPage

--- src/store/users.store.js
+++ src/store/users.store.js
@@ -19,12 +19,36 @@ const loadPreviousPage = async() => {
     state.Users = users;
 }
 
-const onUserChange = async() => {
+/**
+ * 
+ * @param {User} updatedUser 
+ */
+const onUserChange = async(updatedUser) => {
+
+    let wasfound = false;
+
+    state.Users = state.Users.map( user => {
+        if (user.id === updatedUser.id) {
+            wasfound = true;
+            return updatedUser;
+        }
+        return user;
+    })
+
+    if (state.Users.length < 10 && !wasfound) {
+        state.Users.push(updatedUser);
+    }
+
 
 }
 
 const reloadPage = async() => {
-
+    const users = await loadPageByPage( state.CurrentPage );
+    if (users.length === 0) {
+        await loadPreviousPage();
+        return;
+    }
+    state.Users = users;
 }
 
 export default {
